7e4c1fca57fb5f2cee0c2761458fa19161482c28
nikiaprian <101971713+nikiaprian@users.noreply.github.com>
2024-05-19 16:24:35 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react';
-import LoginPage from '../Component/pages/LoginPage.js'; // Sesuaikan path ini dengan struktur folder Anda
+import LoginPage from '/src/Component/pages/LoginPage.js'; // Sesuaikan path ini dengan struktur folder Anda
 
 test('Tombol "Masuk" ada di halaman login', async () => {
   render(<LoginPage />);
