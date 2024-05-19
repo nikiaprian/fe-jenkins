80267b293dd2b4baacce00f76cc321bf16ddff0d
nikiaprian <101971713+nikiaprian@users.noreply.github.com>
2024-05-19 15:36:57 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(
-    <Router>
-      <App />
-    </Router>
-  );
+test('menampilkan tautan learn react', async () => {
+  // Render komponen
+  await act(async () => {
+    render(<App />);
+  });
+
+  // Temukan elemen tautan
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
