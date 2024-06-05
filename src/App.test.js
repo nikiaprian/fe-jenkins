f45884a7ecec8bde4a25aa3de2352f96a055e932
nikiaprian <101971713+nikiaprian@users.noreply.github.com>
2024-06-05 07:51:23 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import { render, waitFor, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { act } from 'react-dom/test-utils';
+import HomePage from './HomePage';
 
 test('renders homepage elements', async () => {
-  await act(async () => {
-    const { container, debug } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+  const { container, debug } = render(<HomePage />);
 
-    // Print the entire container for debugging
-    debug(container);
+  // Print the rendered container for debugging
+  debug(container);
 
-    // Check if the text "Website untuk membantu" is in the document
-    await waitFor(() => {
-      const element = screen.getByText(/Website untuk membantu/);
-      debug(element);  // Add this line
-      expect(element).toBeInTheDocument();
-    });
+  // Check if the text "Website untuk membantu" is in the document
+  await waitFor(() => {
+    const element = screen.getByText(/Website untuk membantu/);
+    debug(element);  // Add this line
+    expect(element).toBeInTheDocument();
   });
 });
-
-
 
 
 // import React from 'react';
