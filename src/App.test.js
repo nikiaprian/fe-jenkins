1798a187d8339e62d3b8a4b383fb70b163d4a639
nikiaprian <101971713+nikiaprian@users.noreply.github.com>
2024-06-05 08:24:28 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import App from './App';
 
-
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   );
-//   // Your test assertions here
-// });
+test('renders learn react link', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  // Your test assertions here
+});
