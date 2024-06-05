6a0523e3e4894666008364c70b678dbf8744e461
nikiaprian <101971713+nikiaprian@users.noreply.github.com>
2024-06-05 07:45:35 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, waitFor, screen } from '@testing-library/react';
-import HomePage from './Component/pages/HomePage.js';
+import HomePage from './Component/pages/HomePage';
 
 test('renders homepage elements', async () => {
   const { container, debug } = render(<HomePage />);
