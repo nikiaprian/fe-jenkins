650d2fc7f9e64bda16386a849afaf91240430257
nikiaprian <101971713+nikiaprian@users.noreply.github.com>
2024-06-05 08:21:15 +0700

    Update HomePage.test.js

--- src/HomePage.test.js
+++ src/HomePage.test.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import HomePage from './Component/pages/HomePage';
 
-test('renders homepage elements', async () => {
-  const { container, debug } = render(
+test('renders homepage elements', () => {
+  render(
     <BrowserRouter>
       <HomePage />
     </BrowserRouter>
   );
 
-  // Print the rendered container for debugging
-  debug(container);
-
-  // Check if the text "Website untuk membantu" is in the document
-  await waitFor(() => {
-    const element = screen.getByText(/Website untuk membantu/);
-    debug(element);  // Add this line
-    expect(element).toBeInTheDocument();
-  });
+  const element = screen.getByText(/Website untuk membantu/);
+  expect(element).toBeInTheDocument();
 });
