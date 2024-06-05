25871887e2ed406e09f52083326fb45e1cdecb30
nikiaprian <101971713+nikiaprian@users.noreply.github.com>
2024-06-05 07:47:40 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Component/pages/HomePage';
 
-test('renders homepage elements', async () => {
-  const { container, debug } = render(<HomePage />);
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <React.Suspense fallback={<div>Loading...</div>}>
+              <HomePage />
+            </React.Suspense>
+          }
+        />
+        {/* routes lainnya */}
+      </Routes>
+    </>
+  );
+}
 
-  // Print the rendered container for debugging
-  debug(container);
-
-  // Check if the text "Website untuk membantu" is in the document
-  await waitFor(() => {
-    const element = screen.getByText(/Website untuk membantu/);
-    debug(element);  // Add this line
-    expect(element).toBeInTheDocument();
-  });
-});
-
-
+export default App;
 
 
 
