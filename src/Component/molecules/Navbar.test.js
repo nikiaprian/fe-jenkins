6a7a559d2a0939189b4313d74373ba72a64356f4
nikiaprian <aprianniki69@gmail.com>
2024-08-15 23:57:17 +0700

    navbaru1

--- src/Component/molecules/Navbar.test.js
+++ src/Component/molecules/Navbar.test.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Navbar from './Navbar';
 import { MemoryRouter } from 'react-router-dom';
+import useAuthStore from '../store/AuthStore';
+
+jest.mock('../store/AuthStore');
 
 test('renders Navbar component', () => {
+  // Mock isLoggedIn state from the auth store
+  useAuthStore.mockReturnValue({
+    isLoggedIn: false,
+    setIsLoggedIn: jest.fn(),
+    setUser: jest.fn(),
+  });
+
   render(
     <MemoryRouter>
       <Navbar />
     </MemoryRouter>
   );
-  // Add your assertions here
+
+  // Assert the presence of "Daftar" and "Masuk" buttons when not logged in
+  expect(screen.getByText(/Daftar/i)).toBeInTheDocument();
+  expect(screen.getByText(/Masuk/i)).toBeInTheDocument();
+
+  // Assert the presence of links in the navbar
+  expect(screen.getByText(/ForumIn/i)).toBeInTheDocument();
+  expect(screen.getByText(/BlogIn/i)).toBeInTheDocument();
+  expect(screen.getByText(/Tentang/i)).toBeInTheDocument();
+  expect(screen.getByText(/FaQ/i)).toBeInTheDocument();
+});
+
+test('renders Navbar component when logged in', () => {
+  // Mock isLoggedIn state from the auth store to be true
+  useAuthStore.mockReturnValue({
+    isLoggedIn: true,
+    setIsLoggedIn: jest.fn(),
+    setUser: jest.fn(),
+  });
+
+  render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>
+  );
+
+  // Assert the presence of "Keluar" button when logged in
+  expect(screen.getByText(/Keluar/i)).toBeInTheDocument();
+  
+  // Assert that "Daftar" and "Masuk" buttons are not rendered
+  expect(screen.queryByText(/Daftar/i)).not.toBeInTheDocument();
+  expect(screen.queryByText(/Masuk/i)).not.toBeInTheDocument();
 });
