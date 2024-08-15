5ce920da3727bfcc08dfeaf4d40bcc5b030b3274
nikiaprian <aprianniki69@gmail.com>
2024-08-16 01:22:55 +0700

    blogtest7

--- src/Component/pages/CreateBlogPage.test.js
+++ src/Component/pages/CreateBlogPage.test.js
@@ -1,6 +1,14 @@
-import { render, screen} from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import CreateBlogPage from './CreateBlogPage';
 import { MemoryRouter } from 'react-router-dom';
+
+jest.mock('axios', () => ({
+  post: jest.fn(() => Promise.resolve({ data: {} })),
+}));
+
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn(),
+}));
 
 test('renders CreateBlogPage with all necessary components', () => {
   render(
@@ -8,21 +16,19 @@ test('renders CreateBlogPage with all necessary components', () => {
       <CreateBlogPage />
     </MemoryRouter>
   );
-  
-  // Memastikan elemen dengan teks yang benar ada
+
   expect(screen.getByLabelText(/Judul Blog/i)).toBeInTheDocument();
   expect(screen.getByLabelText(/Masukkan judul Blog/i)).toBeInTheDocument();
   expect(screen.getByText(/Upload Image Banner Blog/i)).toBeInTheDocument();
   expect(screen.getByLabelText(/Deskripsi/i)).toBeInTheDocument();
-  
-  // Memastikan tombol Submit ada
-  expect(screen.getByRole('button', { name: /Submit/i })).toBeInTheDocument();
-  
-  // // Memastikan input tags bekerja
+
+  const submitButton = screen.getByRole('button', { name: /Submit/i });
+  expect(submitButton).toBeInTheDocument();
+
+  // Jika input tags tidak muncul, coba periksa nama atau labelnya
   // const inputTag = screen.getByRole('textbox', { name: /Tags/i });
   // fireEvent.change(inputTag, { target: { value: 'Test Tag' } });
   // expect(inputTag.value).toBe('Test Tag');
-  
-  // Memastikan pratinjau markdown ada
+
   expect(screen.getByText(/Preview/i)).toBeInTheDocument();
 });
