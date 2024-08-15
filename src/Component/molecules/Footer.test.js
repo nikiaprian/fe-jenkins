2569b4c97d4df7e9a72be7e9e90be06e3f99eb47
nikiaprian <aprianniki69@gmail.com>
2024-08-15 23:45:47 +0700

    footertest3

--- src/Component/molecules/Footer.test.js
+++ src/Component/molecules/Footer.test.js
@@ -9,16 +9,16 @@ test('renders Footer component with expected links', () => {
     </MemoryRouter>
   );
   
-  // Contoh assertions
-  expect(screen.getByText('Features')).toBeInTheDocument();
-  expect(screen.getByText('Help Center')).toBeInTheDocument();
-  expect(screen.getByText('Contact')).toBeInTheDocument();
+  // Memastikan elemen dengan teks yang benar ada
+  expect(screen.getByText('Fitur')).toBeInTheDocument();
+  expect(screen.getByText('Pusat Bantuan')).toBeInTheDocument();
+  expect(screen.getByText('Info Kontak')).toBeInTheDocument();
   
   // Memastikan link tertentu ada dengan href yang benar
-  expect(screen.getByRole('link', { name: /GitHub/i })).toHaveAttribute('href', 'https://github.com/username');
-  expect(screen.getByRole('link', { name: /LinkedIn/i })).toHaveAttribute('href', 'https://linkedin.com/in/username');
+  expect(screen.getByRole('link', { name: /Forum CodeIn/i })).toHaveAttribute('href', '/forum');
+  expect(screen.getByRole('link', { name: /Blog CodeIn/i })).toHaveAttribute('href', '/blog');
 
   // Memastikan ada ikon media sosial tertentu
-  expect(screen.getByTestId('twitter-icon')).toBeInTheDocument();
-  expect(screen.getByTestId('facebook-icon')).toBeInTheDocument();
+  expect(screen.getByAltText('fb')).toBeInTheDocument();
+  expect(screen.getByAltText('twitter-icon')).toBeInTheDocument();
 });
