0d0a75c3ecbfc8dfa7f203cbf48c130361dbaa51
nikiaprian <aprianniki69@gmail.com>
2024-08-15 21:52:54 +0700

    15aug2

--- src/Component/atoms/Button.js
+++ src/Component/atoms/Button.js
@@ -5,7 +5,7 @@ const Button = (props) => {
   return (
     <>
       <button className="shadow-lg px-6 py-1.5 bg-orange-500 rounded-md border-neutral-700 border-2 text-white font-bold hover:bg-orange-600 hover:text-white hover:border-orange-500">
-        Kirim
+      {props.children}
       </button>
     </>
   );
