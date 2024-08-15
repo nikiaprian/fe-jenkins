29824ace433039e37930ff5f77b50c058555769b
nikiaprian <aprianniki69@gmail.com>
2024-08-15 22:29:03 +0700

    15aug5

--- src/Component/molecules/InputFile.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import InputFile from './InputFile';
-
-test('renders InputFile component', () => {
-  render(<InputFile />);
-  const inputElement = document.querySelector('input[type="file"]');
-  expect(inputElement).toBeInTheDocument();
-});
