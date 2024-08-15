8746dd2cd2416f08a630432f5d43ba9cf6888776
nikiaprian <aprianniki69@gmail.com>
2024-08-16 00:51:30 +0700

    inputtest1

--- src/Component/molecules/Navbar.test.js
+++ src/Component/molecules/Navbar.test.js
@@ -1,46 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import useAuthStore from '../store/AuthStore';
 import Navbar from './Navbar';
 
-jest.mock('../store/AuthStore', () => ({
-  __esModule: true,
-  default: () => ({
-    isLoggedIn: true,
-    setIsLoggedIn: jest.fn(),
-    setUser: jest.fn(),
-  }),
-}));
-
-test('shows profile and logout button when logged in', () => {
+test('renders all navigation links', () => {
   render(
     <MemoryRouter>
       <Navbar />
     </MemoryRouter>
   );
 
-  // Assert profile picture and logout button are present
-  expect(screen.getByRole('button', { name: /Keluar/i })).toBeInTheDocument();
-  expect(screen.getByAltText('')).toBeInTheDocument(); // Check profile image
-});
-
-jest.mock('../store/AuthStore', () => ({
-  __esModule: true,
-  default: () => ({
-    isLoggedIn: false,
-    setIsLoggedIn: jest.fn(),
-    setUser: jest.fn(),
-  }),
-}));
-
-test('shows login and register links when not logged in', () => {
-  render(
-    <MemoryRouter>
-      <Navbar />
-    </MemoryRouter>
-  );
-
-  // Assert login and register links are present
-  expect(screen.getByRole('button', { name: /Masuk/i })).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: /Daftar/i })).toBeInTheDocument();
+  // Assert that all links are present
+  const links = ['ForumIn', 'BlogIn', 'Tentang', 'FaQ'];
+  links.forEach((linkText) => {
+    expect(screen.getByText(linkText)).toBeInTheDocument();
+  });
 });
