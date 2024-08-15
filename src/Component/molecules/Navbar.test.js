cf17c840ba94b98d2bc754e0c736116c656a37ee
nikiaprian <aprianniki69@gmail.com>
2024-08-16 00:45:18 +0700

    navbaru9

--- src/Component/molecules/Navbar.test.js
+++ src/Component/molecules/Navbar.test.js
@@ -1,36 +1,17 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Navbar from './Navbar';
 
-// Test untuk Navbar saat pengguna tidak login
-test('renders Navbar component with signup and login buttons when not logged in', () => {
-  render(<Navbar isLoggedIn={false} />);
-  
-  // Mendapatkan semua elemen dengan teks "Daftar"
-  const daftarButtons = screen.getAllByText(/Daftar/i);
-  
-  // Mengecek apakah setidaknya satu elemen "Daftar" ada di dokumen
-  expect(daftarButtons.length).toBeGreaterThan(0);
-  
-  // Melakukan asersi lebih lanjut pada elemen spesifik
-  expect(daftarButtons[0]).toBeInTheDocument();
+test('renders all navigation links', () => {
+  render(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>
+  );
 
-  // Mendapatkan semua elemen dengan teks "Masuk"
-  const masukButtons = screen.getAllByText(/Masuk/i);
-  expect(masukButtons.length).toBeGreaterThan(0);
-  expect(masukButtons[0]).toBeInTheDocument();
-});
-
-// Test untuk Navbar saat pengguna sudah login
-test('renders Navbar component with logout button when logged in', () => {
-  render(<Navbar isLoggedIn={true} />);
-  
-  // Mendapatkan semua elemen dengan teks "Keluar"
-  const keluarButtons = screen.getAllByText(/Keluar/i);
-  
-  // Mengecek apakah setidaknya satu elemen "Keluar" ada di dokumen
-  expect(keluarButtons.length).toBeGreaterThan(0);
-  
-  // Melakukan asersi lebih lanjut pada elemen spesifik
-  expect(keluarButtons[0]).toBeInTheDocument();
+  // Assert that all links are present
+  const links = ['ForumIn', 'BlogIn', 'Tentang', 'FaQ'];
+  links.forEach((linkText) => {
+    expect(screen.getByText(linkText)).toBeInTheDocument();
+  });
 });
