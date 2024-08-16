3d698ab148182fe4f34dc7f4f24fd28923bb7247
nikiaprian <aprianniki69@gmail.com>
2024-08-16 21:31:32 +0700

    blogtest16

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
+// babel.config.js
 module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-react',
+    '@babel/preset-flow',
   ],
   plugins: [
-    "@babel/plugin-transform-runtime",
-  ],
-};
+    'babel-plugin-styled-components',
+    '@babel/plugin-proposal-class-properties',
+  ]
+}
