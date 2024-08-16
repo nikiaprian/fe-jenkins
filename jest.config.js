c99e9c0aed2f66a75612f1e38ccd03bc68023ba4
nikiaprian <aprianniki69@gmail.com>
2024-08-16 21:52:42 +0700

    blogtest21

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
-// jest.config.js
 module.exports = {
-  testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(react-dnd|react-dnd-html5-backend|react-tag-input)/)",
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  
+  testEnvironment: 'jsdom',
 };
